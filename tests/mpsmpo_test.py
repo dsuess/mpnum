751b201662f914d97a65f8bfdc83629ba08b5fb6
Moritz Lange <moritz.lange@uni-ulm.de>
2018-05-09 14:43:09 +0200

    Changed the decorator because there is no parameter rank in test_mpo_to_pmps()

--- tests/mpsmpo_test.py
+++ tests/mpsmpo_test.py
@@ -224,7 +224,8 @@ def test_pmps_to_mpo(nr_sites, local_dim, rank, rgen):
     assert_array_almost_equal(rho_mp, rho_np)
 
 
-@pt.mark.parametrize('nr_sites, local_dim', pt.MP_TEST_PARAMETERS)
+@pt.mark.parametrize('nr_sites, local_dim', [(1, 7), (2, 3), (3, 2), (6, 2),
+                                             (4, 3), (5, 2)])
 def test_mpo_to_pmps(nr_sites, local_dim, rgen):
     pmps = factory.random_mpa(nr_sites, (local_dim, local_dim), 1,
                               dtype=np.complex_, randstate=rgen)
