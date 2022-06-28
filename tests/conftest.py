a32f81c2ac6f3d6252d4df69a7a0b4763a5cf409
Milan Holzaepfel <mail@mjh.name>
2022-06-28 16:11:16 +0200

    Update pytest configuration
    (Tests still do not work due to other problems.)
    
    References:
    * <https://docs.pytest.org/en/7.0.x/deprecations.html#the-strict-command-line-option>
    * <https://docs.pytest.org/en/7.0.x/deprecations.html#pytest-namespace>

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,13 +2,12 @@ import numpy as np
 import pytest
 
 
-def pytest_namespace():
-    return dict(
-        # nr_sites, local_dim, rank
-        MP_TEST_PARAMETERS=[(1, 7, np.nan), (2, 3, 3), (3, 2, 4), (6, 2, 4),
+def pytest_configure():
+    # TODO Move these constants to mpnum._testing or find a better solution.
+    # nr_sites, local_dim, rank
+    pytest.MP_TEST_PARAMETERS=[(1, 7, np.nan), (2, 3, 3), (3, 2, 4), (6, 2, 4),
                             (4, 3, 5), (5, 2, 1)],
-        MP_TEST_DTYPES=[np.float_, np.complex_]
-    )
+    pytest.MP_TEST_DTYPES=[np.float_, np.complex_]
 
 
 @pytest.fixture(scope="module")
